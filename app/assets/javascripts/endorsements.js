8daab52406b73b7915fe4d9841eae6b5333df0b7
Maya Driver <mayadrivermd@gmail.com>
2014-10-02 11:13:19 +0100

    used ajax to allow endorsements to update without refreshing

---
+++ app/assets/javascripts/endorsements.js
@@ -0,0 +1,13 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$('document').ready(function(){
+    $('.endorsement-link').on('click', function(event){
+        var endorsementCount = $(this).siblings('.endorsements_count');
+        event.preventDefault();
+        $.post(this.href, function(response){
+            endorsementCount.text(response.new_endorsement_count);
+        });
+    });
+})
--- spec/features/endorsement_features_spec.rb
+++ spec/features/endorsement_features_spec.rb
@@ -7,7 +7,7 @@ describe 'endorsing reviews' do
         pizza_hut.reviews.create(rating: 3, review: 'Ok')
     end
 
-    it 'can endorse a review updating the review endorsement count' do
+    it 'can endorse a review updating the review endorsement count', js: true do
         visit '/restaurants'
         click_link 'Endorse this review'
         expect(page).to have_content '1 endorsement'
